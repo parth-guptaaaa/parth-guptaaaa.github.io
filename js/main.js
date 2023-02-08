50891519b13e3381eb8e51621aa8fe7dbe4dbf52
Parth Gupta <parthgupta2001@kgpian.iitkgp.ac.in>
2023-02-08 14:12:15 +0530

    how

--- js/main.js
+++ js/main.js
@@ -74,4 +74,18 @@ $('.accordion-header').click(function(){
     $(this).children('span').text('-');
 });
 
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$icon.onclick = () => {
+  $arrow.animate([
+    {left: '0'},
+    {left: '10px'},
+    {left: '0'}
+  ],{
+    duration: 700,
+    iterations: Infinity
+  });
+}
+
 });
